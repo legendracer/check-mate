1bbc3b8ca97d0365891d7d92587a362ba830629c
unknown <anujpreetham@gmail.com>
2025-04-06 00:21:09 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        # just simulate saving data
+        print("Plastic data submitted!")
+    return render_template("index.html")
 
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
